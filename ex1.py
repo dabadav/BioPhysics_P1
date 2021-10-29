dce24f2256c6814a6425f9a9709e97d832e55e20
dantekali <dante.avino@alum.esci.upf.edu>
2021-10-29 08:45:53 +0200

    laptop

--- ex1.py
+++ ex1.py
@@ -1,38 +1,24 @@
-"""
-Determine the list of pairs of residues whose CA atoms are closer than a given distance 
-Parameters: PDB file name, distance.
-"""
-"""Example script for building custom command-lines use argparse"""
-
 import argparse
 import sys
 
 parser = argparse.ArgumentParser(
-                                 prog='ProgName', 
-                                 description='Description of the program'
+                                 prog='s1', 
+                                 description='Determine the list of pairs of residues whose CA atoms are closer than a given distance'
                                  )
 
 # Use add_argument for all needed arguments
 # https://docs.python.org/3/library/argparse.html
 
-# Fornat for a boolean parameter, value stored as True if present, False if not
-parser.add_argument(
-                    '--option1', 
-                    action='store_true', 
-                    dest='var1',
-                    help='ON/OFF option1'
-                    )
-
 # Format for a optional text argument, default values can be indicated.
 parser.add_argument(
-                    '--option2', 
-                    dest='var2',
-                    help='Introduce option2'
+                    '-d',
+                    dest='distance',
+                    help='Maximum distance between CA atoms of residues pairs'
                     )
 
 # Format for a required parameter, call will fail if empty. This only stores the file name, but specifiying type=file, the file is open and contents available. 
-parser.add_argument('required_file',
-                    help='Required file for the program')
+parser.add_argument('pdb_file',
+                    help='Required file (.pdb)')
 
 # Read command line into args
 
@@ -48,8 +34,7 @@ for k, v in vars(args).items():
 print ("\nSettings, again\n---------------")    
 
 #print the variables once assigned
-Variable1 = args.var1
-Variable2 = args.var2
-Variable_Required = args.required_file
+Variable2 = args.distance
+Variable_Required = args.pdb_file
 
-print(Variable1, Variable2, Variable_Required)    
+print(Variable2, Variable_Required)  
